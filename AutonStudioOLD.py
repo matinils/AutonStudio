8f9c985e657014c145f6d05b91ec647ab27a7f65
Matthew Kozlowski <mkozlowski225@gmail.com>
2020-03-05 11:56:46 -0600

    Added velocity choosing

--- AutonStudioOLD.py
+++ AutonStudioOLD.py
@@ -7,12 +7,36 @@ import time
 
 if __name__ == '__main__':
 
+    # Fields used during the loop
+    drivetrain = '[Mechanum with Odometry]'
+    selectingStartPoint = False
+    addingPoint = False
+    addingTurn = False
+    simulating = False
+    pathEditUpdated = False
+    turnEditUpdated = False
+    startPoint_circle = None
+    startPoint_line = None
+    robot_rectangle = None
+    robot_line = None
+    point_lines = []
+    turn_circles = []
+    turnIndicator_circles = []
+    turnIndicator_text = []
+    points = []
+    turns = []
+    convertedPoints = []
+    paths = [None]
+    turnStrings = [None]
+    selectedPathNum = None
+    selectedTurnNum = None
+
     sg.theme('Dark Green')  # please make your windows colorful
     logo = sg.Image('resources/autonStudioLogo.png')
 
+    drivetrain = '[Mechanum with Odometry]'
     drive_selection = [sg.Listbox(['Mechanum with Odometry', 'Mechanum without Odometry', 'H-Drive with Odometry',
-                                 'H-Drive without Odometry'], enable_events=True, key='-DRIVETRAIN_SELECTION-',
-                                  size=(25,4), default_values='Mechanum with Odometry')]
+                                 'H-Drive without Odometry'], enable_events=True, key='-DRIVETRAIN_SELECTION-', size=(25,4), default_values='Mechanum with Odometry')]
 
 
     menu_column = [[sg.Text('\n\n')],
@@ -28,65 +52,17 @@ if __name__ == '__main__':
 
      # f = open("testFile.txt", "x") This can be used to create a file. Very easy. Nice.
 
-    # Fields used during the loop
-    drivetrain = '[Mechanum with Odometry]'
-    selectingStartPoint = False
-    addingPoint = False
-    addingTurn = False
-    simulating = False
-    pathEditUpdated = False
-    turnEditUpdated = False
-    startPoint_circle = None
-    startPoint_line = None
-    robot_rectangle = None
-    robot_line1 = None
-    robot_line2 = None
-    robot_line3 = None
-    robot_line4 = None
-    robot_point = None
-    robot_polygon = None
-    point_lines = []
-    turn_circles = []
-    turnIndicator_circles = []
-    turnIndicator_text = []
-    points = []
-    turns = []
-    convertedPoints = []
-    paths = [None]
-    turnStrings = [None]
-    selectedPathNum = None
-    selectedTurnNum = None
-    startHeading = 0.0
-
-
     studioWindowActive = False
-    configWindowActive = False
     while True:
         event0, values0 = title_window.read()
 
-        print(event0)
+        if event0 == '-DRIVETRAIN_SELECTION-':
+            drivetrain = values0
+
+
 
         if event0 is None or event0 == 'Exit:':
             break
-
-        if event0 == '-DRIVETRAIN_SELECTION-':
-            drivetrain = str(values0)
-
-        if not configWindowActive and event0 == '-CONFIG_BUTTON-':
-            configWindowActive = True
-
-            configLayout = [[sg.Button('Test Button')]]
-            configWindow = sg.Window('Configuration Menu', configLayout)
-
-            while True and configWindowActive:
-                eventC, valuesC = configWindow.Read()
-
-                if eventC is None:
-                    configWindowActive = False
-                    configWindow.Close()
-                    break
-
-
 
         fieldSave = None
         if not studioWindowActive and event0 == '-CONTINUE_BUTTON-':
@@ -128,17 +104,19 @@ if __name__ == '__main__':
             editing_tabGroup = sg.TabGroup(
                 layout=[[sg.Tab(layout=paths_tab, title='Paths'), sg.Tab(layout=turns_tab, title='Turns')]])
 
+            drivetrain = str(drivetrain)
             main_column = [[sg.Button('Set Start Point', key='-START_POINT_BUTTON-')],
                            [sg.Button('Add Point', key='-ADD_POINT_BUTTON-')],
                            [sg.Button('Add Turn', key='-ADD_TURN_BUTTON-')],
                            [sg.Button('Add Robot Operation')],
                            [sg.Button('Simulate Robot Run', key='-SIMULATE_BUTTON-')],
                            [sg.Text('\nEdit Menu:')],
-                           [editing_tabGroup], [sg.Text('Selected Drivetrain: ' + drivetrain[drivetrain.index('[')  : drivetrain.index(']') +1 ])], [sg.Button('Clear Field', key='-CLEAR_FIELD_BUTTON-')]]
+                           [editing_tabGroup], [sg.Text('Selected Drivetrain: ' + drivetrain[drivetrain.index('[')  : drivetrain.index(']') +1 ])],
+                           [sg.Button('Clear Field', key='-CLEAR_FIELD_BUTTON-')]]
 
             layout = [[field, sg.Column(main_column)],
-                      [sg.Button('Back', key='-BACK_BUTTON-'), sg.Button('Exit')]]
-            studio_window = sg.Window('EXPERIMENTAL GUI', layout)
+                      [sg.Button('Back', key='-BACK_BUTTON-')],[sg.Button('Exit')]]
+            studio_window = sg.Window('Main GUI', layout)
 
             studio_window.finalize()
 
@@ -166,7 +144,7 @@ if __name__ == '__main__':
             print(values1)
             print()
 
-            # Exit Condition
+            #Exit Condition
             if event1 is None or event1 == 'Exit':
                 studio_window.close()
                 title_window.close()
@@ -183,7 +161,7 @@ if __name__ == '__main__':
             # Clears all field elements and paths
             if event1 == '-CLEAR_FIELD_BUTTON-' and len(points) > 0:
                 field.delete_figure(robot_rectangle)
-                field.delete_figure(robot_point)
+                field.delete_figure(robot_line)
                 field.delete_figure(startPoint_circle)
                 for tc in turn_circles:
                     field.delete_figure(tc)
@@ -284,7 +262,6 @@ if __name__ == '__main__':
                         points[0] = ([values1['-FIELD-'][0], values1['-FIELD-'][1]])
                     else:
                         points.append([values1['-FIELD-'][0], values1['-FIELD-'][1]])
-                    startHeading = float(hf.clean_coordinates(sg.PopupGetText(message='Enter start heading, 0 is straight up, 90 is to the right, -90 is to the left', title='Heading selection')))
                     selectingStartPoint = False
 
             # Select next point and and it to list of points
@@ -339,68 +316,19 @@ if __name__ == '__main__':
                 addingPoint = False
                 selectingStartPoint = False
             if simulating:
-                prevTurn = None
-                robotCBr = [45, -45]  # Bottom right corner and go clockwise
-                robotCBl = [-45, -45]
-                robotCTl = [-45, 45]
-                robotCTr = [45, 45]
-                robotPolygonPoints = [robotCBr, robotCBl, robotCTl, robotCTr]
-                rot_deltas_final = [[[45], [-45]], [[-45], [-45]], [[-45], [45]], [[45], [45]]]
-                if abs(startHeading - 0.0) > 0:
-                    rot_deltas_final = hf.calculate_rotation_per_frame(points=robotPolygonPoints, angle1=0.0, angle2=startHeading, degrees_per_second=45, frames_per_second=60)
                 for i in range(1, len(points)):
                     deltas = hf.calculate_movement_per_frame(points[i-1], points[i], inches_per_second=48, frames_per_second=60, pixels_per_inch=5)
                     num_movements = math.sqrt((points[i][0] - points[i-1][0])**2 + (points[i][1] - points[i-1][1])**2) / math.hypot(deltas[0], deltas[1])
                     x = points[i-1][0]
                     y = points[i-1][1]
-                    for t in turns:
-                        if t[0] == i - 1:
-                            if prevTurn is None:
-                                angle1 = startHeading
-                            else:
-                                angle1 = prevTurn[1]
-                            robotCBr = [45, -45]  # Bottom right corner and go clockwise
-                            robotCBl = [-45, -45]
-                            robotCTl = [-45, 45]
-                            robotCTr = [45, 45]
-                            robotPolygonPoints = [robotCBr, robotCBl, robotCTl, robotCTr]
-                            rot_deltas = hf.calculate_rotation_per_frame(points=robotPolygonPoints, angle1=angle1, angle2=t[1], degrees_per_second=45, frames_per_second=60)
-                            if abs(float(t[1]) - float(angle1)) > 0:
-                                rot_deltas_final = rot_deltas.copy()
-                            prevTurn = t
-                            for j in range(0, len(rot_deltas[0][0])-1):
-                                start_time = time.time()
-                                field.delete_figure(robot_polygon)
-                                field.delete_figure(robot_point)
-                                robotCBr = [x + rot_deltas_final[0][0][j], y + rot_deltas_final[0][1][j]]  # Bottom right corner and go clockwise
-                                robotCBl = [x + rot_deltas_final[1][0][j], y + rot_deltas_final[1][1][j]]
-                                robotCTl = [x + rot_deltas_final[2][0][j], y + rot_deltas_final[2][1][j]]
-                                robotCTr = [x + rot_deltas_final[3][0][j], y + rot_deltas_final[3][1][j]]
-                                robotPolygonPoints = [robotCBr, robotCBl, robotCTl, robotCTr]
-                                robot_polygon = field.draw_polygon(robotPolygonPoints, line_color='black', line_width='3', fill_color='')
-                                robotLinePoints = [[((robotCTr[0] + robotCBr[0]) / 2.0), ((robotCTr[1] + robotCBr[1]) / 2.0)], [((robotCTr[0] + robotCBr[0]) / 2.0) - 20, ((robotCTr[1] + robotCBr[1]) / 2.0) - 20]]
-                                robot_point = field.draw_point(point=robotLinePoints[0], color='yellow', size=15)
-                                studio_window.refresh()
-                                sleepTime = 1 / 60 - (time.time() - start_time)
-                                if sleepTime < 0:
-                                    sleepTime = 0
-                                time.sleep(sleepTime)
                     for j in range(0, int(num_movements)):
                         start_time = time.time()
                         x += deltas[0]
                         y += deltas[1]
-                        field.delete_figure(robot_polygon)
-                        field.delete_figure(robot_point)
-                        robotCBr = [x + rot_deltas_final[0][0][-1], y + rot_deltas_final[0][1][-1]]  # Bottom right corner and go clockwise
-                        robotCBl = [x + rot_deltas_final[1][0][-1], y + rot_deltas_final[1][1][-1]]
-                        robotCTl = [x + rot_deltas_final[2][0][-1], y + rot_deltas_final[2][1][-1]]
-                        robotCTr = [x + rot_deltas_final[3][0][-1], y + rot_deltas_final[3][1][-1]]
-                        robotPolygonPoints = [robotCBr, robotCBl, robotCTl, robotCTr]
-                        robot_polygon = field.draw_polygon(robotPolygonPoints, line_color='black', line_width='3', fill_color='')
-                        robotPoint = [((robotCTr[0] + robotCBr[0]) / 2.0), ((robotCTr[1] + robotCBr[1]) / 2.0)]
-                        if startHeading == 0 and rot_deltas_final == [[[45], [-45]], [[-45], [-45]], [[-45], [45]], [[45], [45]]]:
-                            robotPoint = [((robotCTr[0] + robotCTl[0]) / 2.0), ((robotCTr[1] + robotCTl[1]) / 2.0)]
-                        robot_point = field.draw_point(point=robotPoint, color='yellow', size=15)
+                        field.delete_figure(robot_rectangle)
+                        field.delete_figure(robot_line)
+                        robot_rectangle = field.draw_rectangle(bottom_right=[x+45, y-45], top_left=[x-45, y+45], line_color='black', line_width=3)
+                        robot_line = field.draw_line([x+45, y], [x+10, y], 'blue', width=4.0)
                         studio_window.refresh()
                         sleepTime = 1/60 - (time.time() - start_time)
                         if sleepTime < 0:
@@ -436,26 +364,19 @@ if __name__ == '__main__':
             # Draw robot on the field and ensures the robot cannot be magically clipping through
             # the field walls (robot starts touching field wall if outside boundary)
             if len(points) > 0:
-                field.delete_figure(robot_polygon)
-                field.delete_figure(robot_point)
-                robotCBr = [45, -45]  # Bottom right corner and go clockwise
-                robotCBl = [-45, -45]
-                robotCTl = [-45, 45]
-                robotCTr = [45, 45]
-                robotPolygonPoints = [robotCBr, robotCBl, robotCTl, robotCTr]
-                rot_deltas_final = [[[45], [-45]], [[-45], [-45]], [[-45], [45]], [[45], [45]]]
-                if abs(startHeading - 0.0) > 0:
-                    rot_deltas_final = hf.calculate_rotation_per_frame(points=robotPolygonPoints, angle1=0.0, angle2=startHeading, degrees_per_second=45, frames_per_second=60)
-                robotCBr = [points[0][0] + rot_deltas_final[0][0][-1], points[0][1] + rot_deltas_final[0][1][-1]]  # Bottom right corner and go clockwise
-                robotCBl = [points[0][0] + rot_deltas_final[1][0][-1], points[0][1] + rot_deltas_final[1][1][-1]]
-                robotCTl = [points[0][0] + rot_deltas_final[2][0][-1], points[0][1] + rot_deltas_final[2][1][-1]]
-                robotCTr = [points[0][0] + rot_deltas_final[3][0][-1], points[0][1] + rot_deltas_final[3][1][-1]]
-                robotPolygonPoints = [robotCBr, robotCBl, robotCTl, robotCTr]
-                robot_polygon = field.draw_polygon(robotPolygonPoints, line_color='black', line_width='3', fill_color='')
-                robotPoint = [((robotCTr[0] + robotCBr[0]) / 2.0), ((robotCTr[1] + robotCBr[1]) / 2.0)]
-                if startHeading == 0:
-                    robotPoint = [((robotCTr[0] + robotCTl[0]) / 2.0), ((robotCTr[1] + robotCTl[1]) / 2.0)]
-                robot_point = field.draw_point(point=robotPoint, color='yellow', size=15)
+                field.delete_figure(robot_rectangle)
+                field.delete_figure(robot_line)
+                if points[0][0] < 45:
+                    points[0][0] = 45
+                if points[0][0] > 675:
+                    points[0][0] = 675
+                if points[0][1] < 45:
+                    points[0][1] = 45
+                if points[0][1] > 675:
+                    points[0][1] = 675
+                bRightRobotRect = [points[0][0] + 45, points[0][1] - 45]
+                tLeftRobotRect = [points[0][0] - 45, points[0][1] + 45]
+                robotLinePoints = [[points[0][0] + 45, points[0][1]], [points[0][0] + 10, points[0][1]]]
 
                 # Draw lines between all points
                 if len(points) > 0:
@@ -478,7 +399,9 @@ if __name__ == '__main__':
                     point_lines[i - 2] = (field.draw_line(points[i - 1], points[i], color=lineColor, width=2.0))
 
 
-
+                robot_rectangle = field.draw_rectangle(bottom_right=bRightRobotRect, top_left=tLeftRobotRect, line_color='black', line_width=3)
+                robot_line = field.draw_line(robotLinePoints[0], robotLinePoints[1], 'blue', width=4.0)
+                field.draw_polygon([[400, 400],[400, 300],[300, 400]], line_color='black', line_width=4, fill_color='')
 
     title_window.close()
 
